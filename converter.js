db46de32bcbfb3e348d8ead72fa0df6495364c38
kacpe01 <kbrodawka20@gmail.com>
2025-09-22 16:19:51 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const AGENTS = {
-        kakobuy: { name: 'Kakobuy', logo: 'https://imgur.com/gJd4wvQ.png', format: (id) => `https://kakobuy.com/item/details?url=https%3A%2F%2Fweidian.com%2Fitem.html%3FitemID%3D${id}` },
-        cnfans: { name: 'CNFans', logo: 'https://imgur.com/6Xjx6fC.png', format: (id) => `https://cnfans.com/product?id=${id}&platform=WEIDIAN` },
-        acbuy: { name: 'ACBuy', logo: 'https://imgur.com/2n38uvs.png', format: (id) => `https://www.acbuy.com/product?id=${id}&source=WD` },
-        lovegobuy: { name: 'Lovegobuy', logo: 'https://imgur.com/rM1gCZN.png', format: (id) => `https://lovegobuy.com/product?id=${id}&shop_type=weidian` },
-        mulebuy: { name: 'Mulebuy', logo: 'https://imgur.com/u4n44B4.png', format: (id) => `https://mulebuy.com/product?id=${id}&platform=WEIDIAN` },
-        hoobuy: { name: 'Hoobuy', logo: 'https://imgur.com/jxc2e7x.png', format: (id) => `https://hoobuy.com/product/2/${id}` }
+        kakobuy: { name: 'Kakobuy', logo: 'https://i.imgur.com/gJd4wvQ.png', format: (id) => `https://kakobuy.com/item/details?url=https%3A%2F%2Fweidian.com%2Fitem.html%3FitemID%3D${id}` },
+        cnfans: { name: 'CNFans', logo: 'https://i.imgur.com/6Xjx6fC.png', format: (id) => `https://cnfans.com/product?id=${id}&platform=WEIDIAN` },
+        acbuy: { name: 'ACBuy', logo: 'https://i.imgur.com/2n38uvs.png', format: (id) => `https://www.acbuy.com/product?id=${id}&source=WD` },
+        lovegobuy: { name: 'Lovegobuy', logo: 'https://i.imgur.com/rM1gCZN.png', format: (id) => `https://lovegobuy.com/product?id=${id}&shop_type=weidian` },
+        mulebuy: { name: 'Mulebuy', logo: 'https://i.imgur.com/u4n44B4.png', format: (id) => `https://mulebuy.com/product?id=${id}&platform=WEIDIAN` },
+        hoobuy: { name: 'Hoobuy', logo: 'https://i.imgur.com/jxc2e7x.png', format: (id) => `https://hoobuy.com/product/2/${id}` }
     };
     
     const getItemId = (url) => {
@@ -29,7 +29,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const handleConvert = () => {
         const url = linkInput.value.trim();
-        if (!url) return;
+        if (!url) {
+            alert("Proszę wkleić link.");
+            return;
+        }
 
         const itemId = getItemId(url);
         if (!itemId) {
